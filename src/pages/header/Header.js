e75b2ec06e634a6125d07d378787de44a890304b
Umesh Gayashan <138327662+UmeshGayashan@users.noreply.github.com>
2024-06-08 21:55:00 +0530

    Styling and page navigation

--- src/pages/header/Header.js
+++ src/pages/header/Header.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import { Link } from 'react-router-dom';
+import "./Header.css";
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <Navbar expand="lg" bg="dark" data-bs-theme="dark" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand >Not-LoggedIn</Navbar.Brand>
+          <Nav className="ml-auto">
+            <Nav.Link as={Link} to="/login" className='nav-link'>Login</Nav.Link>
+            <Nav.Link as={Link} to="/register" className='nav-link'>Signup</Nav.Link>
+          </Nav>
+      </Container>
+    </Navbar>
   )
 }
 
