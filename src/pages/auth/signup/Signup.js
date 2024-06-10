a41d93175dfe5b928f677a1051bbf1de3b46627c
Umesh Gayashan <138327662+UmeshGayashan@users.noreply.github.com>
2024-06-10 09:16:12 +0530

    Signup page Designed

--- src/pages/auth/signup/Signup.js
+++ src/pages/auth/signup/Signup.js
@@ -1,8 +1,48 @@
 import React from 'react'
+import { Button } from 'react-bootstrap'
+import Form from 'react-bootstrap/Form';
 
 function Signup() {
   return (
-    <div>Signup</div>
+    <div>
+      <Form>
+        <h1>Signup</h1>
+        <Form.Group controlId="formBasicEmail">
+            <Form.Label>Email Address</Form.Label>
+            <Form.Control
+              type="email"
+              name="email"
+              placeholder="Enter Email"
+              >
+            </Form.Control>
+        </Form.Group>
+
+        <Form.Group controlId="formBasicName">
+            <Form.Label>Name</Form.Label>
+            <Form.Control
+              type="text"
+              name="name"
+              placeholder="Enter Name"
+              >
+            </Form.Control>
+        </Form.Group>
+
+        <Form.Group controlId="formBasicPassword">
+            <Form.Label>Password Address</Form.Label>
+            <Form.Control
+              type="password"
+              name="password"
+              placeholder="Enter Password"
+              >
+            </Form.Control>
+        </Form.Group>
+
+        <Button variant='primary' type='submit' className='w-100'>
+          Signup
+        </Button>
+
+      </Form>
+    </div>
   )
 }
 
